ff492543ea67d0a2559920cda3199beeb0471237
Prabhat-kr-Tiwari <sonukbc6@gmail.com>
2023-10-08 01:08:26 +0530

    added

--- NodeJsIntegration/server.js
+++ NodeJsIntegration/server.js
@@ -1,39 +1,18 @@
 const express = require('express');
-// const { cond } = require('lodash');
- const { MongoClient } = require('mongodb');
- //const MongoClient=require('mongodb').MongoClient
-
+const { MongoClient } = require('mongodb');
 const app = express();
-
-
 const url = 'mongodb://ecomUser:secret@127.0.0.1:27017/ecom'; // MongoDB connection URI
 let db;
-
  (async () => {
      const client = await MongoClient.connect(url);
      db = client.db('ecom')
  })()
-// MongoClient.connect(url,(error,client)=>{
-//     if(error){
-//         console.log(error)
-//     }
-//     console.log('connected')
-//     db=client.db('ecom')
 
-// })
 
 
 app.get('/', async (req, res) => {
      const data = await db.collection('products').find().toArray()
      res.send(data)
-    // db.collection('products').find().toArray( function(error,result){
-
-    //     if(error){
-    //         console.log(error)
-    //     }
-    //     res.send(result)
-
-    // })
   
 
 });
